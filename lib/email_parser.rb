b85212b8dc8361b950a60d2f100cf69924f4039c
Sharmaine Requerme <srqrme@gmail.com>
2018-08-07 18:00:00 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,9 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-  attr_accessor :email
+  attr_accessor :emails
 
-
+  def initialize(emails)
+    @emails = emails
+  end
 end
